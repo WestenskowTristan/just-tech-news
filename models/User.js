9ba69dde8e385c6563bc085a4453eabfedf4c321
tristan <westenskowtristan122@gmail.com>
2021-04-13 19:06:38 -0600

    adding password validation

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
-const { Hooks } = require("sequelize/types/lib/hooks");
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 //define table columns and configure
 User.init(
@@ -53,6 +57,14 @@ User.init(
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
       },
     },
 
