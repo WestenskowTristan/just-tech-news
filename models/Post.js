205510e70985ee5a086028621b34936eceeb13c4
tristan <westenskowtristan122@gmail.com>
2021-04-20 19:07:27 -0600

    adding stuff

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-
 // create our Post model
 class Post extends Model {
   static upvote(body, models) {
@@ -24,10 +23,27 @@ class Post extends Model {
             "vote_count",
           ],
         ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: [
+              "id",
+              "comment_text",
+              "post_id",
+              "user_id",
+              "created_at",
+            ],
+            include: {
+              model: models.User,
+              attributes: ["username"],
+            },
+          },
+        ],
       });
     });
   }
 }
+
 // create fields/columns for Post model
 Post.init(
   {
